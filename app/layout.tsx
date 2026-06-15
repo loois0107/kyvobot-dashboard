44c1319365fc9fd58d5683cb1a02f22b8fef384c
loois0107 <gyeongjui60@gmail.com>
2026-06-15 12:41:26 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Providers from "../components/Providers";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Providers } from './providers'; // 방금 만든 프로바이더를 불러옵니다.
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Kyvo Dashboard",
-  description: "Manage your discord bot",
+  title: 'KyvoBot Dashboard',
+  description: 'Manage your KyvoBot server',
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body>
-        <Providers>
-          {children}
-        </Providers>
+      <body className={inter.className}>
+        {/* Providers로 감싸주어 빌드 시 useSession 에러를 완전히 방지합니다. */}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
